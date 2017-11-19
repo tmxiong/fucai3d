c004fae5e8560a0e32fd6cd5a5cb582d911a82f4
tmxiong <tmxiong@promote.cache-dns.local>
2017-11-19 23:25:07 +0800

    -

---
+++ views/detail/pkPage.js
@@ -0,0 +1,96 @@
+/**
+ * Created by timxiong on 2017/9/6.
+ */
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    ScrollView,
+    Alert
+} from 'react-native';
+
+import NavBar from '../../component/NavBar';
+import cfn from '../../tools/commonFun'
+import config from '../../config/config'
+import fetchp from '../../tools/fetch-polyfill';
+import urls from '../../config/urls';
+
+export default class jieshaoPage extends Component {
+
+    static defaultProps={
+
+    };
+    constructor(props) {
+        super(props);
+
+        this.id = this.props.navigation.state.params.id;
+        this.name = this.props.navigation.state.params.name;
+        this.img = this.props.navigation.state.params.img;
+        this.state={
+            data:[]
+        }
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    goBack() {
+        this.props.navigation.goBack();
+    }
+
+    getData() {
+        fetchp(urls.getJieshao(this.id),{timeout:5*1000})
+            .then((res)=>res.json())
+            .then((data)=>this.setData(data))
+    }
+
+    setData(data) {
+        this.setState({data:data.result.list[0].decription})
+    }
+
+    render() {
+        return(
+            <View style={styles.container}>
+                <NavBar
+                    middleText={this.name}
+                    leftFn={this.goBack.bind(this)}
+                />
+                <Image style={styles.logo} source={this.img}/>
+                <ScrollView>
+                    <View style={styles.content}>
+                        <Text style={styles.text}>{this.state.data}</Text>
+                    </View>
+                </ScrollView>
+
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+   container: {
+        backgroundColor:'#fff',
+       flex:1,
+       alignItems:'center'
+   },
+   content: {
+       width:cfn.deviceWidth(),
+       padding:cfn.picWidth(20)
+   },
+    text: {
+       color:'#333',
+        lineHeight:25,
+        fontSize:14
+    },
+    logo: {
+        position:'absolute',
+        width:cfn.deviceWidth()-cfn.picWidth(100),
+        height:cfn.deviceWidth()-cfn.picWidth(100),
+        opacity:0.2,
+        top:cfn.deviceWidth()/2 - 50
+    }
+});
